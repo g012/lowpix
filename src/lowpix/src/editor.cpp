8d6f8536d0ad486659b2d55f98302e8c73db0005
paskal <leclerc.pascal@gmail.com>
2016-04-09 00:10:15 +0200

    Added save dialog.

--- src/lowpix/src/editor.cpp
+++ src/lowpix/src/editor.cpp
@@ -16,8 +16,17 @@ static struct LPE
 	bool exit;
 	bool need_save;
 	int pal_c;
-	struct LPEPalNode* pal_l;
+	LPEPalNode* pal_l;
+	LPEPalNode* rem_paln_pending;
 } lpe = { 0 };
+
+enum LPEDialogResult
+{
+	LPE_DIALOG_WAIT,
+	LPE_DIALOG_YES,
+	LPE_DIALOG_NO,
+	LPE_DIALOG_CANCEL,
+};
 
 static LPEPalNode* LPE_AddPalette(struct LPPalette* pal)
 {
@@ -28,8 +37,8 @@ static LPEPalNode* LPE_AddPalette(struct LPPalette* pal)
 }
 static void LPE_RemPalette(LPEPalNode* n)
 {
-	if (n->prev) n->prev->next = n->next; if (n->next) n->next->prev = n->prev;
-	if (n == lpe.pal_l) lpe.pal_l = 0;
+	if (n->prev) n->prev->next = n->next; else lpe.pal_l = n->next;
+	if (n->next) n->next->prev = n->prev;
 	lp_alloc(n->pal, 0); lp_alloc(n, 0);
 	--lpe.pal_c;
 }
@@ -58,7 +67,30 @@ static void LPE_Dialog_SavePalette(LPEPalNode* n)
 {
 	static const char* formats[] = { "*.act", "*.bin", "*.c", "*.gpl", "*.s" };
 	if (const char* fn = tinyfd_saveFileDialog("Save Palette", "", sizeof(formats) / sizeof(*formats), formats, "Palette Files (*.act, *.bin, *.c, *.gpl, *.s)"))
-		if (LPE_SavePalette(n->pal, fn)) n->need_save = false;
+	{
+		if (LPE_SavePalette(n->pal, fn))
+		{
+			n->need_save = false;
+			n->filename = strcpy((char*)lp_alloc(0, strlen(fn)+1), fn);
+		}
+	}
+}
+
+static LPEDialogResult LPE_Dialog_NeedSave(const char* name, const char* content)
+{
+	LPEDialogResult r = LPE_DIALOG_WAIT;
+	if (ImGui::BeginPopupModal(name, 0, ImGuiWindowFlags_AlwaysAutoResize))
+	{
+		ImVec2 size(80, 0);
+		ImGui::Text(content);
+		if (ImGui::Button("YES", size)) { r = LPE_DIALOG_YES; ImGui::CloseCurrentPopup(); }
+		ImGui::SameLine();
+		if (ImGui::Button("NO", size)) { r = LPE_DIALOG_NO; ImGui::CloseCurrentPopup(); }
+		ImGui::SameLine();
+		if (ImGui::Button("CANCEL", size)) { r = LPE_DIALOG_CANCEL; ImGui::CloseCurrentPopup(); }
+		ImGui::EndPopup();
+	}
+	return r;
 }
 
 void LPE_Tick(char* droppedFiles)
@@ -109,9 +141,12 @@ void LPE_Tick(char* droppedFiles)
 		size.y -= pos.y;
 		ImGui::RootDock(pos, size);
 	}
+	lpe.need_save = false;
 
+	LPEPalNode* rem_paln = 0;
 	for (LPEPalNode* paln = lpe.pal_l; paln; paln = paln->next)
 	{
+		static const ImVec2 btn_sz = { 80, 0 };
 		struct LPPalette* pal = paln->pal;
 		char dock_name[256];
 		snprintf(dock_name, sizeof(dock_name), "%s%s##%p", paln->filename ? drpath_file_name(paln->filename) : "<noname>", paln->need_save ? " *" : "", pal);
@@ -120,16 +155,18 @@ void LPE_Tick(char* droppedFiles)
 		{
 			ImGui::PushID(pal);
 
+			if (ImGui::Button("CLOSE", btn_sz)) rem_paln = paln;
 			if (paln->filename)
 			{
-				if (ImGui::Button("SAVE")) { }
 				ImGui::SameLine();
+				if (ImGui::Button("SAVE", btn_sz)) { if (LPE_SavePalette(pal, paln->filename)) paln->need_save = false; }
 			}
-			if (ImGui::Button("SAVE AS")) { LPE_Dialog_SavePalette(paln); }
-
-			if (ImGui::Button("CLONE")) LPE_AddPalette(lp_pal_clone(pal))->need_save = true;
 			ImGui::SameLine();
-			if (ImGui::Button("UNIQUE")) LPE_AddPalette(lp_pal_unique(pal))->need_save = true;
+			if (ImGui::Button("SAVE AS", btn_sz)) { LPE_Dialog_SavePalette(paln); }
+
+			if (ImGui::Button("CLONE", btn_sz)) LPE_AddPalette(lp_pal_clone(pal))->need_save = true;
+			ImGui::SameLine();
+			if (ImGui::Button("UNIQUE", btn_sz)) LPE_AddPalette(lp_pal_unique(pal))->need_save = true;
 
 			uint32_t* c = pal->col;
 			for (uint32_t i = 0; i < pal->col_count; ++i)
@@ -144,5 +181,25 @@ void LPE_Tick(char* droppedFiles)
 			ImGui::PopID();
 		}
 		ImGui::EndDock();
+		if (paln->need_save) lpe.need_save = true;
+	}
+	if (rem_paln)
+	{
+		if (!rem_paln->need_save) LPE_RemPalette(rem_paln);
+		else { lpe.rem_paln_pending = rem_paln; ImGui::OpenPopup("Save Palette ?"); }
+	}
+
+	if (lpe.rem_paln_pending) switch (LPE_Dialog_NeedSave("Save Palette ?", "Current palette is unsaved. Save it first ?"))
+	{
+	case LPE_DIALOG_NO:
+		LPE_RemPalette(lpe.rem_paln_pending);
+	case LPE_DIALOG_CANCEL:
+		lpe.rem_paln_pending = 0;
+		break;
+	case LPE_DIALOG_YES:
+		if (lpe.rem_paln_pending->filename) LPE_SavePalette(lpe.rem_paln_pending->pal, lpe.rem_paln_pending->filename);
+		else LPE_Dialog_SavePalette(lpe.rem_paln_pending);
+		lpe.rem_paln_pending = 0;
+		break;
 	}
 }
